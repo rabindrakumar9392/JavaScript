b9c693814f18d05a369158638fbc1928f437346f
rabindrakumar9392 <rabindrakumar9392@gmail.com>
2026-09-16 13:52:51 +0530

    Create data-types.js

---
+++ 01-JavaScript-Fundamentals/01-Variables-Data-Types/data-types.js
@@ -0,0 +1,11 @@
+let name = "Rabindra";        // String
+let age = 20;                // Number
+let isStudent = true;        // Boolean
+let value = null;            // Null
+let result;                  // Undefined
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof isStudent);
+console.log(typeof value);
+console.log(typeof result);
